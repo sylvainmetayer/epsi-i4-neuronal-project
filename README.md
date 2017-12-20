c1b9ebe8f04f7ce818e82e506938b39832962c77
Sylvain METAYER <sylvain_metayer@outlook.fr>
2017-12-20 16:57:50 +0100

    Add Training & Network classes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # epsi-i4-neuronal-project
+
+Limite : 15/02
+Attendu : Projet + entretien
--- neurons.py
+++ neurons.py
@@ -1,20 +1,66 @@
 #!/usr/bin/python3
+import json
+import random
 
 
 class Neuron:
-    def __init__(self, weight):
-        self.weight = weight  # array
 
-    def activate(self):
-        pass
+    def __init__(self, size):
+        self.weight = [random.randint(-1000, 1000) for item in range(size)]
 
-    def agregate(self):
-        pass
+    def _activate(self, values):
+        value = self._aggregate(values)
+        return 0 if value <= 0 else 1
 
-    def output(self):
-        return self.activate() if self.agregate() else None
+    def _aggregate(self, values):
+        """
+        Somme des entrées * poids
+        :return:
+        """
+        somme = 0
+        for key, value in enumerate(values):
+            somme += int(value) * self.weight[key]
+        return somme
+
+    def output(self, values):
+        return self._activate(values)
 
 
-class Input:
-    def __init__(self, values):
-        self.values = values  # array
+class Network:
+
+    def __init__(self, neuron_number, neuron_size):
+        self.neurons = []
+        for item in range(neuron_number):
+            self.neurons.append(Neuron(neuron_size))
+
+    def run(self, values):
+        output_results = []
+        for key, neuron in enumerate(self.neurons):
+            ouput = str(neuron.output(values))
+            output_results.append(ouput)
+        str_value = "".join([str(o) for o in output_results])
+        binary_value = bin(int(str_value, 2))
+        return binary_value
+
+
+class Training:
+    def __init__(self, expected, values):
+        self.expected = bin(int(expected, 2))
+        self.values = values
+
+    def run(self):
+        network = Network(3, len(values))
+        result = network.run(values)
+        print("----")
+        print("\tResult from training : " + str(result))
+        print("\tExpected result : " + str(expected))
+        print("\tHurray ! " if self.expected == result else "\tBooooh !")
+        print("----")
+
+
+if __name__ == '__main__':
+    json_object = json.load(open("inputs.json"))
+    for expected in json_object:
+        values = json_object[expected]
+        training = Training(expected, values)
+        training.run()
