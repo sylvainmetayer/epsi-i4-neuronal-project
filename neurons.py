cc1807d6b6202415c0dbb9474139f6dc23475438
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-01-18 16:21:34 +0100

    Update

--- neurons.py
+++ neurons.py
@@ -6,15 +6,15 @@ import random
 class Neuron:
 
     def __init__(self, size):
-        self.weight = [random.randint(-1000, 1000) for item in range(size)]
+        self.weights = [random.randint(-1000, 1000) for item in range(size)]
 
     def _activate(self, values):
         value = self._aggregate(values)
         return 0 if value <= 0 else 1
 
     def update_weight(self, delta, taux, values):
-        for key, weight in enumerate(self.weight):
-            self.weight[key] = weight + taux * delta * values[key]
+        for key, weight in enumerate(self.weights):
+            self.weights[key] = weight + taux * delta * values[key]
 
     def _aggregate(self, values):
         """
@@ -23,11 +23,10 @@ class Neuron:
         """
         somme = 0
         for key, value in enumerate(values):
-            somme += int(value) * self.weight[key]
+            somme += int(value) * self.weights[key]
         return somme
 
     def output(self, values):
-        # print(self.weight)
         return self._activate(values)
 
     def backup(self, file):
@@ -44,28 +43,18 @@ class Network:
         for item in range(neuron_number):
             self.neurons.append(Neuron(neuron_size))
 
-    def run(self, values, expected, taux):
+    def run(self, values):
         output_results = []
         for key, neuron in enumerate(self.neurons):
             ouput = str(neuron.output(values))
             output_results.append(ouput)
         str_value = "".join([str(o) for o in output_results])
-        binary_value = bin(int(str_value, 2))
-        self.update_network(binary_value, expected, taux, values)
-        return binary_value
+        return str_value
 
     def update_network(self, training_result, expected, taux, values):
-        str_expected = expected[2:]
-        str_training_result = training_result[2:]
-        while not len(str_expected) == 3:
-            str_expected = str(0) + str_expected
-
-        while not len(str_training_result) == 3:
-            str_training_result = str(0) + str_training_result
-
-        delta = Network.calculate_delta(str_training_result, str_expected)
+        delta = Network.calculate_delta(training_result, expected)
         if delta != 0:
-            self._update_neuron(delta, taux, values)
+            self._update_neurons(delta, taux, values)
 
     @staticmethod
     def calculate_delta(training_result, expected):
@@ -74,41 +63,47 @@ class Network:
             delta += int(int(expected[i]) != int(training_result[i]))
         return delta
 
-    def _update_neuron(self, delta, taux, values):
+    def _update_neurons(self, delta, taux, values):
         for neuron in self.neurons:
             neuron.update_weight(delta, taux, values)
 
 
 class Training:
-    def __init__(self, expected, values, nb_run):
-        self.expected = bin(int(expected, 2))
-        self.network = Network(3, len(values))
-        self.taux = 1
-        self.nb_run = nb_run
 
-    def run(self, values):
-        for i in range(self.nb_run):
-            # print("Iteration {}".format(str(i)))
-            if not self._single_run(values, self.taux, (self.nb_run - 1 == i), i) and self.taux >= 0.001:
-                self.taux -= 0.0001
-            else:
-                # We found it, no need to continue
-                break
+    def __init__(self, letters, network, taux):
+        self.network = network
+        self.taux = taux
+        self.letters = letters
 
-    def _single_run(self, values, taux, last_run, current_run):
-        result = self.network.run(values, self.expected, taux)
-        if last_run or self.expected == result:
-            print("----")
-            print("\tResult from training after {} times  : {} ".format(str(current_run), str(result)))
-            print("\tExpected result : " + str(self.expected))
-            print("\tHurray ! " if self.expected == result else "\tBooooh !")
-            print("----")
-        return self.expected == result
+    def run(self, nb_run):
+        all_letter_indices = list(self.letters.keys())
+
+        for i in range(nb_run):
+            random.shuffle(all_letter_indices)
+
+            for expected in all_letter_indices:
+                letter = self.letters[expected]
+                is_success, training_result = self._single_run(letter, expected)
+                if not is_success:
+                    self.network.update_network(training_result, expected, self.taux, letter)
+
+            if self.taux > 0.01:
+                self.taux -= 0.01
+
+    def final_test(self):
+        for value in self.letters:
+            letter = self.letters[value]
+            is_success, training_result = self._single_run(letter, value)
+            print("Letter {} : {}".format(str(value), "Success" if is_success else "Not a success"))
+
+    def _single_run(self, values, expected):
+        result = self.network.run(values)
+        return result == expected, result
 
 
 if __name__ == '__main__':
-    json_object = json.load(open("inputs.json"))
-    for expected in json_object:
-        item_values = json_object[expected]
-        training = Training(expected, item_values, 1000)
-        training.run(item_values)
+    letters = json.load(open("inputs.json"))
+    network = Network(3, 15)
+    training = Training(letters, network, taux=1)
+    training.run(10000)
+    training.final_test()
